b55ca78170a8f005d576ae612466272abe935466
Jhon-Crow <jhamish@mail.ru>
2024-05-02 17:48:59 +0300

    25 video
    decorator в preview работает
    надо написать функцию обёртку для проброса глобальных переменных

---
+++ config/storybook/preview.tsx
@@ -0,0 +1,24 @@
+import type { Preview } from '@storybook/react';
+import React from 'react';
+
+const preview: Preview = {
+
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+    decorators: [
+        (Story) => (
+            <div style={{ backgroundColor: 'red' }}>
+                {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
+                <Story />
+            </div>
+        ),
+    ],
+};
+
+export default preview;
