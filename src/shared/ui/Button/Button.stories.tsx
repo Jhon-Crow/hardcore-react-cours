1ae81791aa6d8d3a77b70e94d8e4703e7986ad50
Jhon-Crow <jhamish@mail.ru>
2024-05-02 16:13:49 +0300

    25 video 15 min
    сделать импорт глобальных переменных из scss

---
+++ src/shared/ui/Button/Button.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import 'app/styles/index.scss';
+import { Button, ThemeButton } from './Button';
+
+const meta: Meta<typeof Button> = {
+    title: 'shared/Button',
+    component: Button,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    args: { onClick: fn() },
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Clear: Story = {
+    args: {
+        children: 'Text',
+        theme: ThemeButton.CLEAR,
+    },
+};
+
+export const Primary: Story = {
+    args: {
+        children: 'Text',
+        theme: ThemeButton.PRIMARY,
+    },
+};
+
+export const Outline: Story = {
+    args: {
+        children: 'Text',
+        theme: ThemeButton.OUTLINE,
+    },
+};
