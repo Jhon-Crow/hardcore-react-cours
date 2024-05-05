198ab3e7dfbca417d70d1678f09304b8747f0df6
Jhon-Crow <jhamish@mail.ru>
2024-05-05 08:19:27 +0300

    27 video 10 min
    don't know how to fix github action lint:scss

--- src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
+++ src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
@@ -2,8 +2,14 @@ import 'app/styles/index.scss';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-export const RouterDecorator = (story: () => React.FC) => (
+// export const RouterDecorator = (story: () => React.FC) => (
+//     <BrowserRouter>
+//         {story()}
+//     </BrowserRouter>
+// );
+
+export const RouterDecorator = (Story: React.FC) => (
     <BrowserRouter>
-        {story()}
+        <Story />
     </BrowserRouter>
 );
