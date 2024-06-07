591a5d23773956d3cf4040984e176c44370da558
Jhon-Crow <jhamish@mail.ru>
2024-06-07 09:25:47 +0300

    50 video finished
    #status:
    - all good
    
    # eslint fixed
    
    #toDo:
    - tests for slice & services of ArticleDetails.tsx
    
    - skeleton need to correct sizes & gap (difference between skeleton & article content)

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,6 @@ module.exports = {
         'plugin:react/recommended',
         'airbnb',
         'plugin:i18next/recommended',
-        'plugin:storybook/recommended',
-        'plugin:storybook/recommended',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
@@ -30,10 +28,12 @@ module.exports = {
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
         'react/jsx-filename-extension': [
-            2, { extensions: ['.js', '.jsx', '.tsx'] }],
+            2,
+            { extensions: ['.js', '.jsx', '.tsx'] },
+        ],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
-        'no-unused-vars': 'warn',
+        'no-unused-vars': 'off',
         'react/require-default-props': 'off',
         'react/react-in-jsx-scope': 'off',
         'react/jsx-props-no-spreading': 'warn',
@@ -50,10 +50,10 @@ module.exports = {
             },
         ],
         'max-len': ['error', { ignoreComments: true, code: 120 }],
-        'jsx-a11y/click-events-have-key-events': 'off',
         'jsx-a11y/no-static-element-interactions': 'off',
+        'jsx-a11y/click-events-have-key-events': 'off',
         'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-        'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies
+        'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies,
         'no-param-reassign': 'off',
         'no-undef': 'off',
     },
--- src/entities/Country/model/types/country.ts
+++ src/entities/Country/model/types/country.ts
@@ -1,7 +1,7 @@
 export enum Country {
-    "Russia" = "Russia",
-    "Belarus" = "Belarus",
-    "Ukraine" = "Ukraine",
-    "Kazahstan" = "Kazahstan",
-    "Armenia" = "Armenia",
+    'Russia' = 'Russia',
+    'Belarus' = 'Belarus',
+    'Ukraine' = 'Ukraine',
+    'Kazahstan' = 'Kazahstan',
+    'Armenia' = 'Armenia',
 }
