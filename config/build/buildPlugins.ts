05070cc0aa4e5c6b5ffac2d154c9d5d992fdccb2
Jhon-Crow <jhamish@mail.ru>
2024-06-15 17:00:31 +0300

    add copy plugin
    надо сделать страницу редактирования (она же создания статьи) в конце курса

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -25,7 +25,7 @@ export function buildPlugins({
         }),
         new CopyPlugin({
             patterns: [
-                { from: 'public/locales', to: 'buildLocales' },
+                { from: paths.locales, to: paths.buildLocales },
             ],
         }),
     ];
