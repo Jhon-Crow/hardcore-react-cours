7be8ef1f416521c0cc3cd845a6efc61d2807382f
Jhon-Crow <jhamish@mail.ru>
2024-06-16 17:56:00 +0300

    try to fix buildCssLoader

--- config/build/loaders/buildCssLoader.ts
+++ config/build/loaders/buildCssLoader.ts
@@ -4,7 +4,7 @@ export function buildCssLoader(isDev: boolean) {
     return {
         test: /\.s[ac]ss$/i,
         use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            MiniCssExtractPlugin.loader,
             {
                 loader: 'css-loader',
                 options: {
