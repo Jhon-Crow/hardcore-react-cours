dd2cbba02e6de3665d52f3298983d521f2c8304f
Jhon-Crow <jhamish@mail.ru>
2024-06-17 09:55:53 +0300

    del ArticlesPage from AboutPage.tsx

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from 'widgets/Page/ui/Page';
-import {ArticlesPage} from "pages/ArticlesPage";
 
 const AboutPage = () => {
     const { t } = useTranslation('about');
     return (
         <Page>
             {t('О сайте')}
-            <ArticlesPage/>
         </Page>
     );
 };
