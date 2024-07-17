65a54910b88e129eb1015d4a6c62df387051b791
Jhon-Crow <jhamish@mail.ru>
2024-07-17 21:42:25 +0300

    fix typo in index.ts

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -5,7 +5,6 @@ export {
 export type { ArticleDetailsScheme } from './model/types/articleDetailsScheme';
 
 export { ArticleList } from './ui/ArticleList/ArticleList';
-export { ArticleViewSelector } from '../../features/ArticleViewSelector/ui/ArticleViewSelector';
 export { ArticleSortSelector } from './ui/ArticleSortSelector/ArticleSortSelector';
 export { ArticleTypeTabs } from './ui/ArticleTypeTabs/ArticleTypeTabs';
 export { getArticleDetailsData } from './model/selectors/articleDetails';
