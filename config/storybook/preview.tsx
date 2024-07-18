873cb88061b33acb7a0ba83a6a42b14730b4b25d
Jhon-Crow <jhamish@mail.ru>
2024-07-18 04:57:39 +0300

    add addon-themes in storybook (background switcher)

--- config/storybook/preview.tsx
+++ config/storybook/preview.tsx
@@ -1,13 +1,14 @@
 import type { Preview } from '@storybook/react';
 import 'loki/configure-react';
+import { withThemeByClassName } from '@storybook/addon-themes';
 import { TranslationDecorator }
     from '../../src/shared/config/storybook/TranslationDecorator/TranslationDecorator';
 import { StoreDecorator } from '../../src/shared/config/storybook/StoreDecorator/StoreDecorator';
 import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
-import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
-import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from '../../src/shared/const/theme';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 
 const preview: Preview = {
 
@@ -22,11 +23,21 @@ const preview: Preview = {
     },
     decorators: [
         StyleDecorator,
-        ThemeDecorator(Theme.NORMAL),
+        // @ts-ignore
+        ThemeDecorator(Theme),
         RouterDecorator,
         TranslationDecorator,
         StoreDecorator({}),
         SuspenseDecorator,
+        // @ts-ignore
+        withThemeByClassName({
+            themes: {
+                light: Theme.NORMAL,
+                dark: Theme.DARK,
+                yallow: Theme.YALLOW,
+            },
+            defaultTheme: 'dark',
+        }),
     ],
 };
 
