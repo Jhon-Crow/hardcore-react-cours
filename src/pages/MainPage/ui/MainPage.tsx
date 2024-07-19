2c0283133d8334a27a8cb8e551d283909e24cb3e
Jhon-Crow <jhamish@mail.ru>
2024-07-19 17:12:51 +0300

    Circular dependency fix

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
 import { RatingCard } from '@/entities/Rating';
-import { Counter } from '@/entities/Counter';
 
 /* eslint-disable i18next/no-literal-string */
 
@@ -12,7 +11,7 @@ const MainPage = () => {
         <Page
             data-testid="MainPage"
         >
-            <Counter />
+            {/* <Counter /> */}
             <RatingCard
                 title="Как вам статья?"
                 hasFeedback
